7ef1d4a626c335d21a26b2f598186152daddf74e
Jeff Pettyjohn <Jeffrey.Pettyjohn1@T-Mobile.com>
2021-01-27 12:30:53 -0600

    added initial server

---
+++ index.js
@@ -0,0 +1,27 @@
+const http = require("http");
+
+const host = 'localhost';
+const port = 8000;
+
+const requestListener = function (req, res) {
+    res.setHeader("Content-Type", "application/json");
+    res.writeHead(200);
+    res.end(JSON.stringify({ sum: parseUrlAndGetSum(req.url)}));
+};
+
+function parseUrlAndGetSum(url){
+    var sum = 0;
+    var result = url.split("/");
+    for(var i=0; i<result.length;i++){
+        if(!isNaN(Number(result[i]))){
+            sum += Number(result[i]);
+        }
+    }
+
+    return sum;
+}
+
+const server = http.createServer(requestListener);
+server.listen(port, host, () => {
+    console.log(`Server is running on http://${host}:${port}`);
+});
